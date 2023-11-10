8fbe33dd0e1dcba34be4faf2e89ab7d179bb05f7
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2023-11-10 12:19:18 -0300

    Finish PvP logic

--- script.js
+++ script.js
@@ -9,12 +9,52 @@ const GameBoard = function() {
 
     const getSquare = (index) => board[index][0]; 
     const setSquare = (index, content) => {
-        if(getSquare(index).length > 0) return;
+        if(getSquare(index).length > 0) return false;
         board[index] = content; 
         DisplayHandler.setSquare(index, content);
+        return true;
     }
 
     return {getBoard, getSquare, setSquare};
+}();
+
+const MenuHandler = function() {
+    const sections = document.querySelectorAll('section');
+    const aiBtn = document.getElementById('ai-btn');
+    const pvpBtn = document.getElementById('pvp-btn');
+
+    const aiStartBtn = document.getElementById('btn-ai');
+    const pvpNextBtn = document.getElementById('btn-1');
+    const pvpStartBtn = document.getElementById('btn-2');
+
+    const player1Input = document.getElementById('player-1-name');
+    const player2Input = document.getElementById('player-2-name');
+
+    aiBtn.addEventListener("click", () => setMenu("player-creation-ai"));
+    pvpBtn.addEventListener("click", () => setMenu("player-creation-1"));
+
+    pvpNextBtn.addEventListener("click", () => {
+        let player1Name = player1Input.value;
+        if(player1Name == "") {
+            player1Input.value = "Player 1";
+            player1Name = "Player 1";
+        }
+        setMenu("player-creation-2");
+    });
+
+    pvpStartBtn.addEventListener("click", () => {
+        let player2Name = player2Input.value;
+        if(player2Name == "") player2Name = "Player 2";
+        let player1Name = player1Input.value;
+        Game.startGame(player1Name, player2Name);
+    })
+
+    const setMenu = (id) => {
+        sections.forEach((sect) => sect.classList.add('hidden'));
+        const el = document.getElementById(id);
+        el.classList.remove('hidden');
+    }
+    return {setMenu}
 }();
 
 const DisplayHandler = function() {
@@ -24,6 +64,10 @@ const DisplayHandler = function() {
         el.addEventListener("click", () => Game.playTurn(el.id))
     })
 
+    const currentPlayer = document.querySelector('.current-player');
+    const winningPlayer = document.querySelector('.winning-player');
+    const endGameText = document.querySelector('#end-game-text');
+
     const getSquareElement = (index) => document.getElementById(index);
 
     const setSquare = (index, content) => {
@@ -31,25 +75,43 @@ const DisplayHandler = function() {
         el.firstChild.textContent = content;
     }
 
-    const highlightWinner = (combination) => {
-        // TODO
+    const setCurrentPlayer = (name) => currentPlayer.textContent = name;
+
+    const setGameResult = (winner) => {
+        currentPlayer.parentElement.classList.add('hidden');
+        endGameText.classList.remove('hidden');
+        if(winner === false) {
+            // TIE
+            endGameText.textContent = "It's a tie! :("
+            endGameText.classList.add('tie-text');
+        } else {
+            winningPlayer.textContent = winner;
+            endGameText.classList.add('winner-text');
+        }
     }
 
-    return {setSquare, highlightWinner}
+    const highlightWinner = (combination) => {
+        combination.map((i) => {
+            const el = document.getElementById(i);
+            el.classList.add('square-winning');
+        })
+    }
+
+    return {setSquare, highlightWinner, setCurrentPlayer, setGameResult}
 }();
 
-const Player = function(ty, na) {
-    if(ty !== "O" && ty !== "X") return; 
-    const type = ty;
+const Player = function(sy, na) {
+    if(sy !== "O" && sy !== "X") return; 
+    const symbol = sy;
     const name = na;
     let wins = 0;
 
     const getWins = () => wins;
     const addWin = () => wins =+ 1;
-    const getType = () => type;
+    const getSymbol = () => symbol;
     const getName = () => name;
 
-    return {getWins, addWin, getType, getName}
+    return {getWins, addWin, getSymbol, getName}
 }
 
 const Game = function () {
@@ -57,45 +119,55 @@ const Game = function () {
         [0, 3, 6], [0, 1, 2], [2, 5, 8], [6, 7, 8], [1, 4, 7], [3, 4, 5], [6, 4, 2], [0, 4, 8]
     ];
 
-    let currentPlayer = "X";
+    let players = [];
+    let currentPlayerIndex = 0;
     let status = "playing";
 
-    let p1, p2;
-
-    const startGame = () => {
-        p1 = Player("Miguel", "O");
-        p2 = Player("Renzo", "X");
+    const startGame = (player1, player2) => {
+        if(player2 === false) {
+            // AI
+        } else {
+            // PVP
+            const p1 = Player("X", player1);
+            const p2 = Player("O", player2);
+            players = [p1, p2];
+            DisplayHandler.setCurrentPlayer(players[currentPlayerIndex].getName());
+            MenuHandler.setMenu('game');
+        }
     }
 
     const checkWinner = () => {
         let filledCombinations = 0;
         for(combination of winningCombinations) {
-            let countX = 0;
-            let countO = 0;
+            let countP1 = 0;
+            let countP2 = 0;
             for(i of combination) {
                 const sq = GameBoard.getSquare(i);
-                if(sq.toUpperCase() === "X") countX++;
-                if(sq.toUpperCase() === "O") countO++;
-                if(countX === 3 || countO === 3) {
-                    const winner = countX > countO ? "X" : "O";
+                if(sq.toUpperCase() === players[0].getSymbol()) countP1++;
+                if(sq.toUpperCase() === players[1].getSymbol()) countP2++;
+                if(countP1 === 3 || countP2 === 3) {
+                    const winner = countP1 > countP2 ? players[0].getName() : players[1].getName();
                     status = "ended";
-                    return console.log(winner + " wins with combination: " + combination);
+                    DisplayHandler.highlightWinner(combination);
+                    return DisplayHandler.setGameResult(winner);
                 }
-                if(countX + countO === 3) filledCombinations++;
+                if(countP1 + countP2 === 3) filledCombinations++;
             }
         }
         // Tie
         if(filledCombinations === 8) {
-            console.log("Tie");
             status = "ended";
+            return DisplayHandler.setGameResult(false);
         } 
     }
 
     const playTurn = (index) => {
         if(status !== "playing") return;
-        GameBoard.setSquare(index, currentPlayer);
-        currentPlayer = (currentPlayer == "X") ? "O" : "X";
-        checkWinner();
+        if (GameBoard.setSquare(index, players[currentPlayerIndex].getSymbol())) {
+            currentPlayerIndex = (currentPlayerIndex == 0) ? 1 : 0;
+            DisplayHandler.setCurrentPlayer(players[currentPlayerIndex].getName());
+            checkWinner();
+        }
     }
 
 
